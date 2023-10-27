91ca51466d4495c899d47efc9ad218ffea70d73b
Joshua Ji <joshua.ji@jiwhiz.com>
2023-10-27 17:38:56 -0600

    Delete comment

--- include/ASTNode/Expr/ExprNode.h
+++ include/ASTNode/Expr/ExprNode.h
@@ -2,7 +2,7 @@
 #include "ASTNode/ASTNode.h"
 
 // Base class for all expressions
-// Theoretically, this should not be instantiated by itself. Should I make it virtual?
+// Theoretically, this should not be instantiated by itself
 class ExprNode : public ASTNode {
 public:
     std::shared_ptr<Type> type;  // For type checking
