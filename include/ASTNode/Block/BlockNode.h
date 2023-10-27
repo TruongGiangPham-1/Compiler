f3f8457ddbe0936d72c134d8aea25822e7a60db4
Joshua Ji <joshua.ji@jiwhiz.com>
2023-10-27 15:46:32 -0600

    Believe in the BlockNode

--- include/ASTNode/Block/BlockNode.h
+++ include/ASTNode/Block/BlockNode.h
@@ -2,10 +2,8 @@
 
 #include "ASTNode/ASTNode.h"
 
-// right now, this is mainly used as a parent class for loops and conditionals (a node that contains a nested scopee)
+// right now, this is mainly used as a parent class for loops and conditionals (a node that contains a nested scope)
 // but in the future, it can also stand by itself for Block Statements (https://cmput415.github.io/415-docs/gazprea/spec/statements.html#block-statements)
-// Do we need this? Is it find to just use an ASTNode?
-// not sure, but I think it's better to be more explicit
 class BlockNode : public ASTNode {
 public:
     BlockNode(size_t tokenType, int line);
