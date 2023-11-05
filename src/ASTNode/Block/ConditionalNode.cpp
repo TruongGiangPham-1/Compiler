0e77ade17e7e5fd5e04a3e850969218d613dba3b
Joshua Ji <joshua.ji@jiwhiz.com>
2023-11-05 13:56:10 -0700

    More parens

--- src/ASTNode/Block/ConditionalNode.cpp
+++ src/ASTNode/Block/ConditionalNode.cpp
@@ -7,7 +7,7 @@ std::string ConditionalNode::toString() {
 
     // number of blocks is never smaller than number of conditionals
     for (int i = 0; i < conditions.size(); i++) {
-        ret += " (IF " + conditions[i]->toString() + " THEN " + bodies[i]->toString() + ")";
+        ret += " (IF (" + conditions[i]->toString() + ") THEN (" + bodies[i]->toString() + "))";
     }
 
     // if there is an else block
