94350e88af7060da558ef5757f0c906431d083c8
sfedyk <stan.fedyk@truenorthmortgage.ca>
2023-11-05 03:50:30 -0700

    rm

--- include/block/callable.h
+++
@@ -1,7 +0,0 @@
-#include "ASTNode/ASTNode.h"
-#include "ASTNode/Block/BlockNode.h"
-
-class CallableNode : public ASTNode  {
-  std::vector<ASTNode> args;
-  BlockNode body;
-};
--- include/block/procedure.h
+++
@@ -1,5 +0,0 @@
-
-class procedure : public callableNode {
-
-
-};
