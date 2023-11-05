e10f8864e49460d4b6debd69e8289c148ab71dec
Joshua Ji <joshua.ji@jiwhiz.com>
2023-11-05 13:53:20 -0700

    Improve ConditionalNode::toString()

--- src/ASTNode/Block/ConditionalNode.cpp
+++ src/ASTNode/Block/ConditionalNode.cpp
@@ -3,5 +3,16 @@
 ConditionalNode::ConditionalNode(int line){}
 
 std::string ConditionalNode::toString() {
-    return "Conditional";
+    std::string ret = "Conditional";
+
+    // number of blocks is never smaller than number of conditionals
+    for (int i = 0; i < conditions.size(); i++) {
+        ret += " (IF " + conditions[i]->toString() + " THEN " + bodies[i]->toString() + ")";
+    }
+
+    // if there is an else block
+    if (bodies.size() > conditions.size()) {
+        ret += " (ELSE " + bodies[bodies.size() - 1]->toString() + ")";
+    }
+    return ret;
 }
