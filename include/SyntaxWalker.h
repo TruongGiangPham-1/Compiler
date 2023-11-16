5dbbf607c11b0b1e0eb5d96dbee9267acb2232f1
Joshua Ji <joshua.ji@jiwhiz.com>
2023-11-16 11:06:34 -0700

    Check for globals that aren't const
    also implement all our block stuff

--- include/SyntaxWalker.h
+++ include/SyntaxWalker.h
@@ -10,8 +10,29 @@
 namespace gazprea {
 
     class SyntaxWalker : public ASTWalker {
+    private:
+        // the entire file is wrapped inside a `block` scope
+        // thus, it's easier to just count the scope depth
+        // global scope is depth == 1
+        int scopeDepth;
+
+        bool inGlobalScope();
+        std::string debugGlobalScope();
     public:
         SyntaxWalker();
+
+        // declarations
+        virtual std::any visitDecl(std::shared_ptr<DeclNode> tree);
+
+        // === SCOPES ===
+        virtual std::any visitBlock(std::shared_ptr<BlockNode>tree);
+        virtual std::any visitConditional(std::shared_ptr<ConditionalNode> tree);
+        virtual std::any visitInfiniteLoop(std::shared_ptr<InfiniteLoopNode> tree);
+        virtual std::any visitPredicatedLoop(std::shared_ptr<PredicatedLoopNode> tree);
+        virtual std::any visitPostPredicatedLoop(std::shared_ptr<PostPredicatedLoopNode> tree);
+
+        virtual std::any visitFunction(std::shared_ptr<FunctionNode> tree);
+        virtual std::any visitProcedure(std::shared_ptr<ProcedureNode> tree);
     };
 
 }
