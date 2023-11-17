8ddfb0ae8ef335e01e76e3204d4b1e476039cfc7
Joshua Ji <joshua.ji@jiwhiz.com>
2023-11-17 06:41:25 -0700

    chore: add `override` to methods
    i guess it's better?

--- include/SyntaxWalker.h
+++ include/SyntaxWalker.h
@@ -34,21 +34,21 @@ namespace gazprea {
         SyntaxWalker();
 
         // declarations
-        std::any visitDecl(std::shared_ptr<DeclNode> tree);
+        std::any visitDecl(std::shared_ptr<DeclNode> tree) override;
 
         // === SCOPES ===
-        std::any visitBlock(std::shared_ptr<BlockNode>tree);
-        std::any visitConditional(std::shared_ptr<ConditionalNode> tree);
-        std::any visitInfiniteLoop(std::shared_ptr<InfiniteLoopNode> tree);
-        std::any visitPredicatedLoop(std::shared_ptr<PredicatedLoopNode> tree);
-        std::any visitPostPredicatedLoop(std::shared_ptr<PostPredicatedLoopNode> tree);
+        std::any visitBlock(std::shared_ptr<BlockNode>tree) override;
+        std::any visitConditional(std::shared_ptr<ConditionalNode> tree) override;
+        std::any visitInfiniteLoop(std::shared_ptr<InfiniteLoopNode> tree) override;
+        std::any visitPredicatedLoop(std::shared_ptr<PredicatedLoopNode> tree) override;
+        std::any visitPostPredicatedLoop(std::shared_ptr<PostPredicatedLoopNode> tree) override;
 
-        std::any visitFunction(std::shared_ptr<FunctionNode> tree);
-        std::any visitProcedure(std::shared_ptr<ProcedureNode> tree);
+        std::any visitFunction(std::shared_ptr<FunctionNode> tree) override;
+        std::any visitProcedure(std::shared_ptr<ProcedureNode> tree) override;
 
         // Expr
-        std::any visitCall(std::shared_ptr<CallNode> tree);
-        std::any visitVector(std::shared_ptr<VectorNode> tree);
+        std::any visitCall(std::shared_ptr<CallNode> tree) override;
+        std::any visitVector(std::shared_ptr<VectorNode> tree) override;
     };
 
 }
