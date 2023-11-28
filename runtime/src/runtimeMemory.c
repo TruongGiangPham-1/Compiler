ae7f725a4c072066b713c5cc17f59f0de05aefc9
sfedyk <stan.fedyk@truenorthmortgage.ca>
2023-11-28 12:32:43 -0700

    runtime promotion

--- runtime/src/runtimeMemory.c
+++ runtime/src/runtimeMemory.c
@@ -21,7 +21,7 @@ typedef struct list {
   int currentSize;
   commonType** values; // list of values
 } list;
-
+commonType* promotion(commonType* from, commonType* to);
 // allocate some memory for a new commontype
 commonType* allocateCommonType(void* value, enum TYPE type);
 commonType* allocateListOfSize(int size);
@@ -64,13 +64,15 @@ commonType* copyCommonType(commonType* copyFrom) {
 }
 
 void assignByReference(commonType* dest, commonType* from) {
+  commonType* promotedVal = promotion(from, dest);
+
   if (isCompositeType(dest->type)) {
     deallocateList(dest->value);
   } else {
     free(dest->value);
   }
 
-  dest->value = copyValue(from);
+  dest->value = promotedVal;
 }
 
 commonType* allocateCommonType(void* value, enum TYPE type) {
