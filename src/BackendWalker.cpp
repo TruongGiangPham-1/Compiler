124ee3ed2651a5b729f44ff2abd9a7a32acf5d71
sfedyk <stan.fedyk@truenorthmortgage.ca>
2023-12-06 12:44:08 -0700

    inference

--- src/BackendWalker.cpp
+++ src/BackendWalker.cpp
@@ -206,14 +206,18 @@ std::any BackendWalker::visitType(std::shared_ptr<TypeNode> tree) {
       case TUPLE: 
       {
           auto mtree = std::dynamic_pointer_cast<TupleTypeNode>(tree);
-   
+          int index = 1;
           std::vector<mlir::Value> children;
           for (auto child : mtree->getTypes()) {
 
+            auto rhsItem = codeGenerator.indexCommonType(*(this->inferenceContext.end()-1), codeGenerator.generateValue(index));
+
+            this->inferenceContext.push_back(rhsItem);
             auto val = std::any_cast<mlir::Value>(walk(child));
+            this->inferenceContext.pop_back();
 
             children.push_back(val);
-            
+            index ++;
           }
           return codeGenerator.generateValue(children);
         }
